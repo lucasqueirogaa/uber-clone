85baf1106ec4acc7956bb3002a2a6de35fc04799
Lucas Queiroga <lucasqueirogadev@gmail.com>
2022-12-28 21:15:42 -0300

    Finalização da configuração do redux

---
+++ slices/navSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null,
+  distination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducer: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestinationm, setTravelTimeInformation } =
+  navSlice.actions;
+
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
