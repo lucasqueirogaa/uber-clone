d5268381998749113fa450447799d6b26dae421d
Lucas Queiroga <lucasqueirogadev@gmail.com>
2022-12-29 14:40:50 -0300

    Configuração da API de mapa do google, tendo pesquisa com autoComplete e retorno nessa pesquisa

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,10 +2,12 @@ import React from "react";
 import { View, Text, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const HomeScreen = () => {
   return (
-    <View style={[tw`pt-10 pl-5 bg-white`, { flex: 1 }]}>
+    <View style={[tw`pt-10 px-5 bg-white`, { flex: 1 }]}>
       <View>
         <Image
           style={{
@@ -17,6 +19,32 @@ const HomeScreen = () => {
             uri: "https://links.papareact.com/gzs",
           }}
         />
+
+        <GooglePlacesAutocomplete
+          nearbyPlacesAPI={GOOGLE_MAPS_APIKEY}
+          placeholder="Where From?"
+          debouce={400}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "pt-BR",
+          }}
+          minLength={2}
+          enablePoweredByContainer={false}
+          returnKeyType={"search"}
+          onPress={(data, details = null) => {
+            console.log({ data });
+            console.log({ details });
+          }}
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+        />
+
         <NavOptions />
       </View>
     </View>
