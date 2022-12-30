e6d256ccfb80a09a2c89ac893729bf596552becf
Lucas Queiroga <lucasqueirogadev@gmail.com>
2022-12-30 11:46:26 -0300

    Criação da base de seleção do destino. Seleciona o destino e temos navegação para a página de seleção de corrida.

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,16 +1,40 @@
-import { Text, View } from "react-native";
 import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { View } from "react-native";
+
 import tw from "tailwind-react-native-classnames";
+
 import Map from "../components/Map";
+import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
+  const Stack = createNativeStackNavigator();
+
   return (
     <View style={[tw`pt-10 bg-white`, { flex: 1 }]}>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
 
-      <View style={tw`h-1/2`}></View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
     </View>
   );
 };
