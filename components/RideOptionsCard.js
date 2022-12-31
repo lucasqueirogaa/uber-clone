e302b9aa3131b409f32a9239304188a810de377a
Lucas Queiroga <lucasqueirogadev@gmail.com>
2022-12-30 23:34:54 -0300

    Finalização de boa parte das viagens, criando já a lista de carros e a base para viagem

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,10 +1,74 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  FlatList,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 const RideOptionsCard = () => {
+  const navigation = useNavigation();
+
   return (
-    <View>
-      <Text>Pick a ride</Text>
+    <View style={tw`bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("NavigateCard");
+          }}
+          style={[tw`absolute top-3 left-5 z-50 p-3 rounded-full`]}
+        >
+          <Icon name="chevron-left" type="fontawesome" />
+        </TouchableOpacity>
+        <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
+      </View>
+      <FlatList
+        data={data}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <TouchableOpacity>
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: item.image }}
+            />
+
+            <View>
+              <Text>{item.title}</Text>
+              <Text>Travel Time...</Text>
+            </View>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 };
--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,19 +1,34 @@
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { View } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 
 import tw from "tailwind-react-native-classnames";
 
 import Map from "../components/Map";
 import NavigateCard from "../components/NavigateCard";
 import RideOptionsCard from "../components/RideOptionsCard";
+import { Icon } from "@rneui/base";
+import { useNavigation } from "@react-navigation/native";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
 
   return (
     <View style={[tw`bg-white`, { flex: 1 }]}>
       <View style={tw`h-1/2`}>
+        <View>
+          <TouchableOpacity
+            onPress={() => {
+              navigation.navigate("HomeScreen");
+            }}
+            style={[
+              tw`absolute top-8 left-3 z-50 p-1 rounded-full bg-gray-300`,
+            ]}
+          >
+            <Icon name="chevron-left" type="fontawesome" />
+          </TouchableOpacity>
+        </View>
         <Map />
       </View>
 
